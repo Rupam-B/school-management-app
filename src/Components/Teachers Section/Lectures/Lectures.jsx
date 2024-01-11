8bd0cf018f840fcb04d672d4683cd0c53495189f
Rupam-B <rupam.banerje@gmail.com>
2024-01-11 05:28:40 -0800

    bus location Features added22

--- src/Components/Teachers Section/Lectures/Lectures.jsx
+++ src/Components/Teachers Section/Lectures/Lectures.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './LectureStyle.css'
+import { Link } from 'react-router-dom'
 
 const Lectures = () => {
   return (
-    <div>Lectures</div>
+    <div className='Lecture-Section-main-div'>
+      <div className='Lecture-Section-sub-div'>
+        <div className='Lecture-Section-top-div'>
+          <Link to={'/TeachersHome'} style={{ textDecoration: 'none' }}><i className="fa-solid fa-angle-left"></i></Link>
+          <h2>Lectures</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
