2a37f1bb4132f3c0ec017edcf62d3e3c8bba9237
Rupam-B <rupam.banerje@gmail.com>
2024-01-14 21:04:57 -0800

    Teacher student section differentiated14

--- src/Components/Calender/CalenderSection.jsx
+++ src/Components/Calender/CalenderSection.jsx
@@ -1,14 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './CalenderStyle.css'
 
 const CalenderSection = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    
+    const handleStartMonthChange = () => {
+        setIsLoading(true)
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1500);
+    };
+
+
     return (
         <div className='Calender-Section-main-div'>
             <div className='Calender-Section-sub-div'>
                 <div className='Calender-Section-top-div'>
                     <Link to={'/TeachersHome'} style={{ textDecoration: 'none' }}><i className="fa-solid fa-angle-left"></i></Link>
-                    <h2>Calender</h2>
+                    <h2>Holiday Calender</h2>
+                </div>
+                <div className='Calender-Section-month-select-div'>
+                    <select onChange={handleStartMonthChange} name="" id="">
+                        <option value="">Jan</option>
+                        <option value="">Feb</option>
+                        <option value="">Mar</option>
+                        <option value="">Apr</option>
+                        <option value="">May</option>
+                        <option value="">June</option>
+                        <option value="">July</option>
+                        <option value="">Aug</option>
+                        <option value="">Sep</option>
+                        <option value="">Oct</option>
+                        <option value="">Nov</option>
+                        <option value="">Dec</option>
+                    </select>
+
+                </div>
+
+                <div className='Calender-Section-mid-div'>
+                    {
+                        isLoading?(
+                            <div className='Calender-Section-mid-div-Loading'>
+                        <h4>Loading...</h4>
+                    </div>
+                        )
+                        :
+                        (
+                            <div className='Calender-Section-mid-div-content'>
+                        <div style={{borderLeft:'4px solid skyblue'}} className='Calender-Section-mid-div-content-holidays'>
+                            <p>15th January 2024</p>
+                            <h6>Pongal</h6>
+                        </div>
+                        <div style={{borderLeft:'4px solid green'}} className='Calender-Section-mid-div-content-holidays'>
+                            <p>26th January 2024</p>
+                            <h6>Republic Day</h6>
+                        </div>
+                        <div style={{borderLeft:'4px solid red'}} className='Calender-Section-mid-div-content-holidays'>
+                            <p>14th Feb 2024</p>
+                            <h6>Vasant Panchami</h6>
+                        </div>
+                        <div style={{borderLeft:'4px solid orange'}} className='Calender-Section-mid-div-content-holidays'>
+                            <p>15th January 2024</p>
+                            <h6>Pongal</h6>
+                        </div>
+                        <div style={{borderLeft:'4px solid blue'}} className='Calender-Section-mid-div-content-holidays'>
+                            <p>15th January 2024</p>
+                            <h6>Pongal</h6>
+                        </div>
+                        <div style={{borderLeft:'4px solid skyblue'}} className='Calender-Section-mid-div-content-holidays'>
+                            <p>15th January 2024</p>
+                            <h6>Pongal</h6>
+                        </div>
+                        <div style={{borderLeft:'4px solid skyblue'}} className='Calender-Section-mid-div-content-holidays'>
+                            <p>15th January 2024</p>
+                            <h6>Pongal</h6>
+                        </div>
+                    </div>
+
+                        )
+
+                    }
                 </div>
             </div>
         </div>
--- src/Components/Library/LibrarySection.jsx
+++ src/Components/Library/LibrarySection.jsx
@@ -1,14 +1,184 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './LibraryStyle.css'
 
 const LibrarySection = () => {
+    const [searchDiv, setSearchDiv]= useState(false)
+    const [showResultStatus, setShoeResultStatus]= useState(true)
+    const [searchValue, setSearchValue]= useState('')
+    
+
+
+    const handleSearchFunction =(e)=>{
+        setSearchDiv(true)
+        setSearchValue(e.target.value)
+    }
+    const handleCloseSearchFunction =()=>{
+        setSearchDiv(false)
+        setSearchValue('')
+    }
+
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setShoeResultStatus(!showResultStatus)
+        },1000)
+         // eslint-disable-next-line 
+    },[searchValue])
     return (
         <div className='Library-Section-main-div'>
             <div className='Library-Section-sub-div'>
                 <div className='Library-Section-top-div'>
                     <Link to={'/TeachersHome'} style={{ textDecoration: 'none' }}><i className="fa-solid fa-angle-left"></i></Link>
                     <h2>Library</h2>
+                </div>
+                <div className='Library-Section-top-lower-div'>
+                    <input value={searchValue} onChange={handleSearchFunction} type="text" name='serachLibrary' placeholder='search books in Library'/>
+                </div>
+                <div className={searchDiv?'Library-Section-top-lower-show-search-result-div':'Library-Section-top-lower-hide-search-result-div'}>
+                <i onClick={handleCloseSearchFunction} className="fa-solid" style={{color:'red',fontSize:'1.1rem'}}>^</i>
+
+                {!showResultStatus?(
+                    <h3>Searching</h3>
+                ):(
+                    <h3>No Results Found</h3>
+                )}
+                
+                </div>
+                <div className='Library-Section-mid-div'>
+                    <div className='Library-Section-mid-div-inner'>
+                        <div className='Library-Section-mid-div-inner-head'>
+                            <p>Books Issued</p>
+                            <p>Date</p>
+                            <p className='Library-Section-mid-div-inner-head-R-NR'>
+                                <span>R</span>
+                                <span>NR</span>
+                            </p>
+                        </div>
+                        <div className='Library-Section-mid-div-inner-content'>
+                        <div className='Library-Section-bottom-div-individual-inner'>
+                            <div className='Library-Section-bottom-div-individual-inner-sub'>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data'>
+                                    <p>Social Studies Class 5th</p>
+                                </div>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data-two'>
+                                    <p>23/01/2024</p>
+                                </div>
+                            </div>
+                            <div className='Library-Section-bottom-div-individual-inner-sub-bottom'>
+                            <i className="fa-solid fa-circle" style={{color:'transparent'}}></i>
+                            <i className="fa-solid fa-check" style={{color:'red',fontSize:'1.1rem'}}></i>
+                            </div>
+                        </div>
+
+                        </div>
+                        <div className='Library-Section-mid-div-inner-content'>
+                        <div className='Library-Section-bottom-div-individual-inner'>
+                            <div className='Library-Section-bottom-div-individual-inner-sub'>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data'>
+                                    <p>Maths Class 5th</p>
+                                </div>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data-two'>
+                                    <p>13/01/2024</p>
+                                </div>
+                            </div>
+                            <div className='Library-Section-bottom-div-individual-inner-sub-bottom'>
+                            <i className="fa-solid fa-circle" style={{color:'transparent'}}></i>
+                            <i className="fa-solid fa-check" style={{color:'red',fontSize:'1.1rem'}}></i>
+                            </div>
+                        </div>
+
+                        </div>
+                        <div className='Library-Section-mid-div-inner-content'>
+                        <div className='Library-Section-bottom-div-individual-inner'>
+                            <div className='Library-Section-bottom-div-individual-inner-sub'>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data'>
+                                    <p>Science 9th</p>
+                                </div>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data-two'>
+                                    <p>03/01/2024</p>
+                                </div>
+                            </div>
+                            <div className='Library-Section-bottom-div-individual-inner-sub-bottom'>
+                            <i className="fa-solid fa-check" style={{color:'green',fontSize:'1.1rem'}}></i>
+                            <i className="fa-solid fa-circle" style={{color:'transparent'}}></i>
+                            </div>
+                        </div>
+
+                        </div>
+                        <div className='Library-Section-mid-div-inner-content'>
+                        <div className='Library-Section-bottom-div-individual-inner'>
+                            <div className='Library-Section-bottom-div-individual-inner-sub'>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data'>
+                                    <p>Sanskrit Class 7th</p>
+                                </div>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data-two'>
+                                    <p>23/12/2023</p>
+                                </div>
+                            </div>
+                            <div className='Library-Section-bottom-div-individual-inner-sub-bottom'>
+                            <i className="fa-solid fa-circle" style={{color:'transparent'}}></i>
+                            <i className="fa-solid fa-check" style={{color:'red',fontSize:'1.1rem'}}></i>
+                            </div>
+                        </div>
+
+                        </div>
+                        <div className='Library-Section-mid-div-inner-content'>
+                        <div className='Library-Section-bottom-div-individual-inner'>
+                            <div className='Library-Section-bottom-div-individual-inner-sub'>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data'>
+                                    <p>Ecology Class 11th</p>
+                                </div>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data-two'>
+                                    <p>13/12/2023</p>
+                                </div>
+                            </div>
+                            <div className='Library-Section-bottom-div-individual-inner-sub-bottom'>
+                            <i className="fa-solid fa-check" style={{color:'green',fontSize:'1.1rem'}}></i>
+                            <i className="fa-solid fa-circle" style={{color:'transparent'}}></i>
+                            </div>
+                        </div>
+
+                        </div>
+                        <div className='Library-Section-mid-div-inner-content'>
+                        <div className='Library-Section-bottom-div-individual-inner'>
+                            <div className='Library-Section-bottom-div-individual-inner-sub'>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data'>
+                                    <p>Social Studies Class 5th</p>
+                                </div>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data-two'>
+                                    <p>23/01/2024</p>
+                                </div>
+                            </div>
+                            <div className='Library-Section-bottom-div-individual-inner-sub-bottom'>
+                            <i className="fa-solid fa-circle" style={{color:'transparent'}}></i>
+                            <i className="fa-solid fa-check" style={{color:'red',fontSize:'1.1rem'}}></i>
+                            </div>
+                        </div>
+
+                        </div>
+                        <div className='Library-Section-mid-div-inner-content'>
+                        <div className='Library-Section-bottom-div-individual-inner'>
+                            <div className='Library-Section-bottom-div-individual-inner-sub'>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data'>
+                                    <p>Social Studies Class 5th</p>
+                                </div>
+                                <div className='Library-Section-bottom-div-individual-inner-sub-data-two'>
+                                    <p>23/01/2024</p>
+                                </div>
+                            </div>
+                            <div className='Library-Section-bottom-div-individual-inner-sub-bottom'>
+                            <i className="fa-solid fa-circle" style={{color:'transparent'}}></i>
+                            <i className="fa-solid fa-check" style={{color:'red',fontSize:'1.1rem'}}></i>
+                            </div>
+                        </div>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div className='Library-Section-space-adder-div'>
+
                 </div>
             </div>
         </div>
