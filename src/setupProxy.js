c0d7cb32dc79ce32e1599aed1e5c21a939fab9d1
Rupam-B <rupam.banerje@gmail.com>
2024-02-19 02:51:16 -0800

    moresection added3

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  app.use('/api', createProxyMiddleware({
-    target: 'https://eslate.siddhisoftwares.com',
-    changeOrigin: true,
-  }));
-};
+// module.exports = function (app) {
+//   app.use('/api', createProxyMiddleware({
+//     target: 'https://eslate.siddhisoftwares.com',
+//     changeOrigin: true,
+//   }));
+// };
