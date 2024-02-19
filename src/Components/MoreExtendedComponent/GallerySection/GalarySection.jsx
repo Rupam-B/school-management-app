8b15f7fbb6d0504e8ed2fdc19fe4f8e35e63e8e8
Rupam-B <rupam.banerje@gmail.com>
2024-02-19 04:58:25 -0800

    moresection added6

--- src/Components/MoreExtendedComponent/GallerySection/GalarySection.jsx
+++ src/Components/MoreExtendedComponent/GallerySection/GalarySection.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+// import React, { useEffect, useState } from 'react'
+// import { Link } from 'react-router-dom'
 import axios from 'axios'
 import './GalarySectionStyle.css'
 const GalarySection = () => {
-    const [storedDatas, setStoredDatas] = useState([])
-    const [selectedImage, setSelectedImage] = useState(null);
+    // const [storedDatas, setStoredDatas] = useState([])
+    // const [selectedImage, setSelectedImage] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -20,7 +21,7 @@ const GalarySection = () => {
               const data = response.data;
               if (data) {
                 console.log(data)
-                setStoredDatas(data);
+                // setStoredDatas(data);
               }
             } else {
               throw new Error("Network response was not ok");
@@ -42,15 +43,15 @@ const GalarySection = () => {
   //     window.open(pdfLink);
   // };
 
-  const openModal = (image) => {
-    setSelectedImage(image);
-    console.log("modal clicked")
-    console.log(selectedImage)
-  };
+  // const openModal = (image) => {
+  //   setSelectedImage(image);
+  //   console.log("modal clicked")
+  //   console.log(selectedImage)
+  // };
 
-  const closeModal = () => {
-    setSelectedImage(null);
-  };
+  // const closeModal = () => {
+  //   setSelectedImage(null);
+  // };
 
 //   const openPdf = (pdfLink) => {
 //     window.open(pdfLink);
@@ -59,39 +60,8 @@ const GalarySection = () => {
 
  return (
         <div className='Achivement-section-main-div'>
-            <div style={{paddingBottom:'20vh'}} className='Achivement-section-sub-div'>
-                <div className='Achivement-section-top-div'>
-                    <Link to={'/MoreExtended'} style={{ textDecoration: 'none' }}><i className="fa-solid fa-angle-left"></i></Link>
-                    <h2>Galary</h2>
-                </div>
-                <div className='Achivement-section-mid-div'>
-                    <div className='Achivement-section-mid-div-header'>
-                        <h5>Event</h5>
-                        {/* <h5>File</h5> */}
-                    </div>
-                    {storedDatas&&storedDatas.map((items,index)=>(
-                    <div key={index} className='Galary-section-mid-div-content'>
-                        <div className='Galary-section-mid-div-content-one'><p>{items.title}</p></div>
-                        <div  className='Galary-section-mid-div-content-two'>{items.images&&items.images.map((imgs,index)=>(
-                          <div key={index} onClick={() => openModal(imgs)}>
-                          {/* <div key={index} onClick={() => openPdf(imgs)}> */}
-                            <img className='Galary-section-mid-div-images' src={imgs} alt="" />
-                          </div>
-
-                        ))} </div>
-                    </div>
-                    )) }
-                    {selectedImage && (
-                             <div className="Show-Image">
-                                 <span className="Show-Image-close" onClick={closeModal}>&times;</span>
-                                <div className="Show-Image-sub">
-                                  <img className="Show-Image-modal-image" src={selectedImage} alt="" />
-                                </div>
-                              </div>
-                    )} 
-
-                </div>
-            </div>
+          <h1>Gallery</h1>
+            
         </div>
     )
 }
